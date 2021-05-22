9712b910939fe137c587c35fd7614ea16c618093
Agneta <agnetagsu@yahoo.es>
2021-05-22 20:18:08 +0200

    Agregar archivos de proyecto.

---
+++ Flujo de datos - C2 Ej5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Flujo_de_datos___C2_Ej5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un día de la semana");
+            string dia = Console.ReadLine();
+           
+            switch(dia)
+            {
+                case "lunes":
+                    Console.WriteLine("{0} no es fin de semana",dia);
+                    break;
+
+                case "martes":
+                    Console.WriteLine("{0} no es fin de semana",dia);
+                    break;
+
+                case "miercoles":
+                    Console.WriteLine("{0} no es fin de semana",dia);
+                    break;
+
+                case "jueves":
+                    Console.WriteLine("{0} no es fin de semana",dia);
+                    break;
+
+                case "viernes":
+                    Console.WriteLine("{0} no es fin de semana",dia);
+                    break;
+
+                case "sabado":
+                    Console.WriteLine("{0} es fin de semana",dia);
+                    break;
+
+                case "domingo":
+                    Console.WriteLine("{0} es fin de semana",dia);
+                    break;
+
+                default:
+                    Console.WriteLine("{0} no corresponde a ningún día de la semana",dia);
+                    break;
+
+            }
+
+
+        }
+    }
+}
